fe19f238136d5f8d06e314699566655c38c5aa5c
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-20 20:19:17 +0500

    Teacher and Gender implemented

--- LMS.Domen/Entities/Teacher.cs
+++ LMS.Domen/Entities/Teacher.cs
@@ -2,5 +2,11 @@
 {
     public class Teacher
     {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public Gender? Gender { get; set; }
+        public string Address { get; set; }
+        public string AddressTemporary { get; set; }
     }
 }
