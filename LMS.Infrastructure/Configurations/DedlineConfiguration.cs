1075bd40f3bff11393324b6309c4a3dbeffe0d68
Sardor <107931170+SardorSohinazarov@users.noreply.github.com>
2023-10-21 21:16:03 +0500

    Dedline Configuration implemented (#9)

---
+++ LMS.Infrastructure/Configurations/DedlineConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using LMS.Domen.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LMS.Infrastructure.Configurations
+{
+    public class DedlineConfiguration : IEntityTypeConfiguration<Deadline>
+    {
+        public void Configure(EntityTypeBuilder<Deadline> builder)
+        {
+            builder.HasKey(dedline => dedline.Id);
+
+            builder.Property(dedline => dedline.MaxGrade)
+                .IsRequired();
+
+            builder.Property(dedline => dedline.CreatedDate)
+                .IsRequired()
+                .HasDefaultValue(DateTime.Now);
+            
+            builder.Property(dedline => dedline.StartDate)
+                .IsRequired();
+            
+            builder.Property(dedline => dedline.ExpiredDate)
+                .IsRequired();
+
+            builder.HasMany(dedline => dedline.Grades)
+                .WithOne(grade => grade.Deadline);
+        }
+    }
+}
