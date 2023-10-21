df3aff5d2646325a697dbdda6584a40b16cd4f94
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-21 20:35:35 +0500

    Lesson Configuration created

---
+++ LMS.Infrastructure/Configurations/LessonConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using LMS.Domen.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LMS.Infrastructure.Configurations
+{
+    public class LessonConfiguration : IEntityTypeConfiguration<Lesson>
+    {
+        public void Configure(EntityTypeBuilder<Lesson> builder)
+        {
+            builder.HasKey(lesson => lesson.Id);
+
+            builder.Property(lesson => lesson.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+            
+            builder.Property(lesson => lesson.CourseId)
+                .IsRequired();
+
+            builder.Property(lesson => lesson.Date)
+                .IsRequired();
+
+            builder.HasMany(lesson => lesson.NBs)
+                .WithOne(nb => nb.Lesson);
+        }
+    }
+}
