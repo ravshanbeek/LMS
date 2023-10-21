631af4e870fdf0cba49b05fdc3fbf0a0333bc339
Sardor <107931170+SardorSohinazarov@users.noreply.github.com>
2023-10-21 21:15:48 +0500

    NbConfiguration Created (#8)

---
+++ LMS.Infrastructure/Configurations/NBConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using LMS.Domen.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LMS.Infrastructure.Configurations
+{
+    public class NBConfiguration : IEntityTypeConfiguration<NB>
+    {
+        public void Configure(EntityTypeBuilder<NB> builder)
+        {
+            builder.HasKey(nb => nb.Id);
+
+            builder.Property(nb => nb.IsAvailable)
+                .HasDefaultValue(true);
+
+            builder.Property(nb => nb.LessonId)
+                .IsRequired();
+
+            builder.Property(nb => nb.StudentId)
+                .IsRequired();
+        }
+    }
+}
