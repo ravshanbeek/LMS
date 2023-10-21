1108c44f5b412ad0fe511cba47e872aca7316fa2
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-21 21:19:53 +0500

    Migrated

--- LMS.Infrastructure/Migrations/20231021161904_Configurations.Designer.cs
+++ LMS.Infrastructure/Migrations/20231021161904_Configurations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LMS.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LMS.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231021161904_Configurations")]
+    partial class Configurations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.12");
