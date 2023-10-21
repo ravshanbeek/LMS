bfb32203688569cef5b531023707401bf00121a2
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-21 21:05:51 +0500

    Student Configuration implemented

---
+++ LMS.Infrastructure/Configurations/StudentConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using LMS.Domen.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LMS.Infrastructure.Configurations
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasKey(student => student.Id);
+
+            builder.Property(student => student.FullName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(student => student.Gender)
+                .IsRequired();
+
+            builder.Property(student => student.Address)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(student => student.GroupId)
+                .IsRequired();
+        }
+    }
+}
