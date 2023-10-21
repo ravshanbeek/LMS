f52ed20571870ee8417c4799cd833aba0e76e7c1
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-21 21:15:02 +0500

    Course Configuration Implemented

---
+++ LMS.Infrastructure/Configurations/CourseConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using LMS.Domen.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LMS.Infrastructure.Configurations
+{
+    public class CourseConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.HasKey(course => course.Id);
+
+            builder.Property(course => course.Name)
+                .IsRequired()
+                .HasMaxLength(15);
+
+            builder.Property(course => course.TeacherId)
+                .IsRequired();
+
+            builder.Property(course => course.SubjectId)
+                .IsRequired();
+
+            builder.HasMany(course => course.Lessons)
+                .WithOne(lesson => lesson.Course);
+        }
+    }
+}
