db8c44d01b4d29c0226bcedbef6cac119965ec60
Sardor <107931170+SardorSohinazarov@users.noreply.github.com>
2023-10-21 21:16:18 +0500

    Grade,Student Configuration implemented (#10)
    * Grade Configuration implemented
    
    * Student Configuration implemented

---
+++ LMS.Infrastructure/Configurations/GradeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using LMS.Domen.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LMS.Infrastructure.Configurations
+{
+    public class GradeConfiguration : IEntityTypeConfiguration<Grade>
+    {
+        public void Configure(EntityTypeBuilder<Grade> builder)
+        {
+            builder.HasKey(grade => grade.Id);
+
+            builder.Property(grade => grade.GradeValue)
+                .IsRequired()
+                .HasDefaultValue(0);
+
+            builder.Property(grade => grade.DedlineId)
+                .IsRequired();
+
+            builder.Property(grade => grade.StudentId)
+                .IsRequired();
+        }
+    }
+}
