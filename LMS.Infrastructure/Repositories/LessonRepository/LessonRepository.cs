a3ef7b3bb659a2f91a6798d4a3f62087a6d6bff5
Sardor <107931170+SardorSohinazarov@users.noreply.github.com>
2023-10-23 11:53:44 +0500

    Users/sardor sohinazarov/repositories (#23)
    * Course Repo Created
    
    * Dedline Repo Created
    
    * Grade Repo Created
    
    * Lesson Repo Created

---
+++ LMS.Infrastructure/Repositories/LessonRepository/LessonRepository.cs
@@ -0,0 +1,11 @@
+﻿using LMS.Domen.Entities;
+using LMS.Infrastructure.Contexts;
+
+namespace LMS.Infrastructure.Repositories;
+
+public class LessonRepository : GenericRepository<Lesson, Guid>, ILessonRepository
+{
+    public LessonRepository(AppDbContext appDbContext) : base(appDbContext)
+    {
+    }
+}
