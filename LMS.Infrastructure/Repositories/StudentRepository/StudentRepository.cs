fa77975ebfd4c8b76ea5ed6e67493aff48fc46c8
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-23 11:58:40 +0500

    Student repo created

---
+++ LMS.Infrastructure/Repositories/StudentRepository/StudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using LMS.Domen.Entities;
+using LMS.Infrastructure.Contexts;
+
+namespace LMS.Infrastructure.Repositories;
+
+public class StudentRepository : GenericRepository<Student, Guid>, IStudentRepository
+{
+    public StudentRepository(AppDbContext appDbContext) : base(appDbContext)
+    {
+    }
+}
