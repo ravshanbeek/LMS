0d56caf903d3303fcc0c22853b644b6646d1fe8e
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-23 11:41:02 +0500

    Course Repo Created

---
+++ LMS.Infrastructure/Repositories/CourseRepository/CourseRepository.cs
@@ -0,0 +1,11 @@
+﻿using LMS.Domen.Entities;
+using LMS.Infrastructure.Contexts;
+
+namespace LMS.Infrastructure.Repositories;
+
+public class CourseRepository : GenericRepository<Course, Guid>, ICourseRepository
+{
+    public CourseRepository(AppDbContext appDbContext) : base(appDbContext)
+    {
+    }
+}
