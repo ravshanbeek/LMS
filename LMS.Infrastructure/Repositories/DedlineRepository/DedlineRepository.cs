1d3d8549b8dae2a8b6652f85c0601ce55c55ae93
Sardor <107931170+SardorSohinazarov@users.noreply.github.com>
2023-10-23 11:47:21 +0500

    Users/sardor sohinazarov/repositories (#21)
    * Course Repo Created
    
    * Dedline Repo Created

---
+++ LMS.Infrastructure/Repositories/DedlineRepository/DedlineRepository.cs
@@ -0,0 +1,11 @@
+﻿using LMS.Domen.Entities;
+using LMS.Infrastructure.Contexts;
+
+namespace LMS.Infrastructure.Repositories;
+
+public class DedlineRepository : GenericRepository<Deadline, Guid>, IDedlineRepository
+{
+    public DedlineRepository(AppDbContext appDbContext) : base(appDbContext)
+    {
+    }
+}
