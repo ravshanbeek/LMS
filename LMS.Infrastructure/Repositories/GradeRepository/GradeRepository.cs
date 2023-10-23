3d0edead84f377662e226d5e6b811dfbdaf1827c
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-23 11:50:14 +0500

    Grade Repo Created

---
+++ LMS.Infrastructure/Repositories/GradeRepository/GradeRepository.cs
@@ -0,0 +1,11 @@
+﻿using LMS.Domen.Entities;
+using LMS.Infrastructure.Contexts;
+
+namespace LMS.Infrastructure.Repositories;
+
+public class GradeRepository : GenericRepository<Grade, Guid>, IGradeRepository
+{
+    public GradeRepository(AppDbContext appDbContext) : base(appDbContext)
+    {
+    }
+}
