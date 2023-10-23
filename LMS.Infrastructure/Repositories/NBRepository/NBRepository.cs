9c913c93a2ff180663c91eaacb8c5f43a9e2012a
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-23 11:56:04 +0500

    NB repo Created

---
+++ LMS.Infrastructure/Repositories/NBRepository/NBRepository.cs
@@ -0,0 +1,11 @@
+﻿using LMS.Domen.Entities;
+using LMS.Infrastructure.Contexts;
+
+namespace LMS.Infrastructure.Repositories;
+
+public class NBRepository : GenericRepository<NB, Guid>, INBRepository
+{
+    public NBRepository(AppDbContext appDbContext) : base(appDbContext)
+    {
+    }
+}
