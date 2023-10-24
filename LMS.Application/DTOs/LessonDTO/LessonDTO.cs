0758e3776c7dca2a808d77bdb2e19637aeb77b75
Sardor <107931170+SardorSohinazarov@users.noreply.github.com>
2023-10-25 01:57:57 +0500

    LessonDto implemented (#31)

---
+++ LMS.Application/DTOs/LessonDTO/LessonDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LMS.Application.DTOs;
+
+public record LessonDTO(
+    Guid id,
+    string name,
+    string lecture,//aniqmas sal boshqacha bo'lishiyam mumkin
+    DateTime date,
+    Guid courseId
+    );
---
+++ LMS.Application/DTOs/LessonDTO/LessonForCreation.cs
@@ -0,0 +1,8 @@
+﻿namespace LMS.Application.DTOs;
+
+public record LessonForCreation(
+    string name,
+    string lecture,//aniqmas sal boshqacha bo'lishiyam mumkin
+    DateTime date,
+    Guid courseId
+    );
