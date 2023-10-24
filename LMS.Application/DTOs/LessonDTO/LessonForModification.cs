49263d92e2a376a5d5bd2bf0d87de958851209c2
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-25 01:57:16 +0500

    LessonDto implemented

---
+++ LMS.Application/DTOs/LessonDTO/LessonForModification.cs
@@ -0,0 +1,8 @@
+﻿namespace LMS.Application.DTOs;
+
+public record LessonForModification(
+    string name,
+    string lecture,//aniqmas sal boshqacha bo'lishiyam mumkin
+    DateTime date,
+    Guid courseId
+    );
