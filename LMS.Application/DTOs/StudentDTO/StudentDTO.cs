34c673781c61da6ecf3ac38af89d6e3ad725b1b4
Sardor <107931170+SardorSohinazarov@users.noreply.github.com>
2023-10-25 02:17:47 +0500

    StudentDTO implemented (#32)

---
+++ LMS.Application/DTOs/StudentDTO/StudentDTO.cs
@@ -0,0 +1,14 @@
+﻿using LMS.Domen.Enums;
+
+namespace LMS.Application.DTOs;
+
+public record StudentDTO(
+    Guid id,
+    string fullName,
+    DateTime birthdate,
+    Gender gender,
+    string address,
+    string addressTemporary,
+    string username,
+    Guid groupId
+    );
---
+++ LMS.Application/DTOs/StudentDTO/StudentForCreation.cs
@@ -0,0 +1,15 @@
+﻿using LMS.Domen.Enums;
+
+namespace LMS.Application.DTOs;
+
+public record StudentForCreation(
+    string fullName,
+    DateTime birthdate,
+    Gender gender, 
+    string address,
+    string addressTemporary,
+    string username,
+    //string salt, // balki xatodur
+    //string hashedPassword,
+    Guid groupId
+    );
