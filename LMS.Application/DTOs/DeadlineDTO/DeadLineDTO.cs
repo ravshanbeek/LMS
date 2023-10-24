53f6dac387f65215ee24118289e38729e9315cf8
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-25 01:32:37 +0500

    GradeDTO implemented

---
+++ LMS.Application/DTOs/DeadlineDTO/DeadLineDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LMS.Application.DTOs;
+
+public record DeadLineDTO(
+    float maxGrade,
+    DateTime startDate,
+    DateTime expiredDate,
+    Guid courseId,
+    ICollection<Guid>? grades
+    );
---
+++ LMS.Application/DTOs/DeadlineDTO/DeadlineForModification.cs
@@ -0,0 +1,8 @@
+﻿namespace LMS.Application.DTOs;
+
+public record DeadlineForModification(
+    float maxGrade,
+    DateTime startDate,
+    Guid courseId,
+    ICollection<Guid>? grades
+    );
