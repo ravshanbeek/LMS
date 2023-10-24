c4025b3ebafb8d6d7c464297707aab25d7b15afc
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-25 01:48:51 +0500

    GroupDTO implemented

---
+++ LMS.Application/DTOs/GroupDTO/GroupDTO.cs
@@ -0,0 +1,10 @@
+﻿using LMS.Domen.Entities;
+
+namespace LMS.Application.DTOs;
+
+public record GroupDTO(
+    string name,
+    int level,
+    ICollection<Student>? students,
+    ICollection<Course>? courses
+    );
---
+++ LMS.Application/DTOs/GroupDTO/GroupForModification.cs
@@ -0,0 +1,10 @@
+﻿using LMS.Domen.Entities;
+
+namespace LMS.Application.DTOs;
+
+public record GroupForModification(
+    string name,
+    int level,
+    ICollection<Student>? students,
+    ICollection<Course>? courses
+    );
