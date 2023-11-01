1ef312f55ee7430a95a5ff8e5b8fc44004bfa924
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-11-01 21:38:42 +0500

    LessonService implemented

---
+++ LMS.Application/Services/Lessons/ILessonService.cs
@@ -0,0 +1,12 @@
+﻿using LMS.Application.DTOs;
+
+namespace LMS.Application.Services.Lessons;
+
+public interface ILessonService
+{
+    ValueTask<LessonDTO> CreateLessonAsync(LessonForCreation lessonForCreation);
+    IQueryable<LessonDTO> RetrieveLessons();
+    ValueTask<LessonDTO> RetrieveLessonByIdAsync(Guid lessonId);
+    ValueTask<LessonDTO> ModifyLessonAsync(LessonForModification lessonForModification);
+    ValueTask<LessonDTO> RemoveLessonAsync(Guid lessonId);
+}
---
+++ LMS.Application/Services/Lessons/LessonService.cs
@@ -0,0 +1,55 @@
+﻿using LMS.Application.DTOs;
+using LMS.Domen.Entities;
+using LMS.Infrastructure.Repositories;
+using Mapster;
+
+namespace LMS.Application.Services.Lessons;
+
+public class LessonService : ILessonService
+{
+    private readonly ILessonRepository _lessonRepository;
+
+    public LessonService(ILessonRepository lessonRepository)
+        => _lessonRepository = lessonRepository;
+
+    public async ValueTask<LessonDTO> CreateLessonAsync(LessonForCreation lessonForCreation)
+    {
+        var lesson = lessonForCreation.Adapt<Lesson>();
+        var addedLesson = await _lessonRepository.InsertAsync(lesson);
+
+        return addedLesson.Adapt<LessonDTO>();
+    }
+
+    public async ValueTask<LessonDTO> ModifyLessonAsync(LessonForModification lessonForModification)
+    {
+        var lesson = await _lessonRepository.SelectByIdAsync(lessonForModification.id);
+        //validate
+        lesson = lessonForModification.Adapt(lesson);
+
+        return lesson.Adapt<LessonDTO>();
+    }
+
+    public async ValueTask<LessonDTO> RemoveLessonAsync(Guid lessonId)
+    {
+        var lesson = await _lessonRepository.SelectByIdAsync(lessonId);
+        //validate
+        var deletedLesson = await _lessonRepository.DeleteAsync(lesson);
+
+        return deletedLesson.Adapt<LessonDTO>();
+    }
+
+    public async ValueTask<LessonDTO> RetrieveLessonByIdAsync(Guid lessonId)
+    {
+        var lesson = await _lessonRepository.SelectByIdAsync(lessonId);
+        //validate
+
+        return lesson.Adapt<LessonDTO>();
+    }
+
+    public IQueryable<LessonDTO> RetrieveLessons()
+    {
+        var lessons = _lessonRepository.SelectAll();
+
+        return lessons.Select(lesson => lesson.Adapt<LessonDTO>());
+    }
+}
