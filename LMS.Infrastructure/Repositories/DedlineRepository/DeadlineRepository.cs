0c6ce5b6bf05a870bc123531ff6010daf2e3c011
Sardor <107931170+SardorSohinazarov@users.noreply.github.com>
2023-11-01 21:23:43 +0500

    Users/sardor sohinazarov/service dedline (#43)
    * DedlineService Created and IDedlineRepository changed to IDeadlineRepository
    
    * Deadline Service implemented

---
+++ LMS.Infrastructure/Repositories/DedlineRepository/DeadlineRepository.cs
@@ -0,0 +1,11 @@
+﻿using LMS.Domen.Entities;
+using LMS.Infrastructure.Contexts;
+
+namespace LMS.Infrastructure.Repositories;
+
+public class DeadlineRepository : GenericRepository<Deadline, Guid>, IDeadlineRepository
+{
+    public DeadlineRepository(AppDbContext appDbContext) : base(appDbContext)
+    {
+    }
+}
