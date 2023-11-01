b269c16d889117418ed4fb8494b48fbda8b76c5d
Sardor <107931170+SardorSohinazarov@users.noreply.github.com>
2023-11-02 01:20:52 +0500

    Task Grade Service implemented and namespaces , folders changed grade to TaskGrade (#45)

---
+++ LMS.Application/Services/TaskGrades/ITaskGradeService.cs
@@ -0,0 +1,13 @@
+﻿using LMS.Application.DTOs;
+
+namespace LMS.Application.Services.TaskGrades;
+
+public interface ITaskGradeService
+{
+    //create qilish uchun aniq bo'ladimi yo'qmi bilmadm
+    ValueTask<TaskGradeDTO> ModifyTaskGradeAsync(TaskGradeForModificationStudent taskGradeForModificationStudent);
+    ValueTask<TaskGradeDTO> ModifyTaskGradeAsync(TaskGradeForModificationTeacher taskGradeForModificationTeacher);
+    IQueryable<TaskGradeDTO> RetrieveTaskGrades();
+    ValueTask<TaskGradeDTO> RetrieveTaskGradeByIdAsync(Guid taskGradeId);
+    ValueTask<TaskGradeDTO> RemoveTaskGradeAsync(Guid taskGradeId);
+}
