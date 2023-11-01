a6e1d376b61dc44797afca92a90e063622f28c53
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-11-02 02:14:03 +0500

    StudentService implemented

---
+++ LMS.Application/Services/Students/IStudentService.cs
@@ -0,0 +1,12 @@
+﻿using LMS.Application.DTOs;
+
+namespace LMS.Application.Services.Students;
+
+public interface IStudentService
+{
+    ValueTask<StudentDTO> CreateStudentAsync(StudentForCreation studentForCreation);
+    IQueryable<StudentDTO> RetrieveStudents();
+    ValueTask<StudentDTO> RetrieveStudentByIdAsync(Guid studentId);
+    ValueTask<StudentDTO> ModifyStudentAsync(StudentForModification studentForModification);
+    ValueTask<StudentDTO> RemoveStudentAsync(Guid studentId);
+}
