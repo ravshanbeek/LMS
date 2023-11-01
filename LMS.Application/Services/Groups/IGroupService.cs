87f9520042771c9857cc6d30ccaf4cff82450447
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-11-02 01:57:51 +0500

    GroupService implemented

---
+++ LMS.Application/Services/Groups/IGroupService.cs
@@ -0,0 +1,12 @@
+﻿using LMS.Application.DTOs;
+
+namespace LMS.Application.Services.Groups;
+
+public interface IGroupService
+{
+    ValueTask<GroupDTO> CreateGroupAsync(GroupForCreation groupForCreation);
+    IQueryable<GroupDTO> RetrieveGroups();
+    ValueTask<GroupDTO> RetrieveGroupByIdAsync(Guid groupId);
+    ValueTask<GroupDTO> ModifyGroupAsync(GroupForModification groupForModification);
+    ValueTask<GroupDTO> RemoveGroupAsync(Guid groupId);
+}
