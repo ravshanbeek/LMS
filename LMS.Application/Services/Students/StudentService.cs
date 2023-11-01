090da7c2fab95d7bce7266acd10e814fb45b0c7a
Sardor <107931170+SardorSohinazarov@users.noreply.github.com>
2023-11-02 02:15:01 +0500

    Users/sardor sohinazarov/service student (#49)
    * GroupService implemented
    
    * StudentService implemented

---
+++ LMS.Application/Services/Students/StudentService.cs
@@ -0,0 +1,56 @@
+﻿using LMS.Application.DTOs;
+using LMS.Domen.Entities;
+using LMS.Infrastructure.Repositories;
+using Mapster;
+
+namespace LMS.Application.Services.Students;
+
+public class StudentService : IStudentService
+{
+    private readonly IStudentRepository _studentRepository;
+
+    public StudentService(IStudentRepository studentRepository)
+        => _studentRepository = studentRepository;
+
+    public async ValueTask<StudentDTO> CreateStudentAsync(StudentForCreation studentForCreation)
+    {
+        var student = studentForCreation.Adapt<Student>();
+        var addedStudent = await _studentRepository.InsertAsync(student);
+
+        return addedStudent.Adapt<StudentDTO>();
+    }
+
+    public async ValueTask<StudentDTO> ModifyStudentAsync(StudentForModification studentForModification)
+    {
+        var student = await _studentRepository.SelectByIdAsync(studentForModification.id);
+        //validate
+        student = studentForModification.Adapt(student);
+        var updatedStudent = await _studentRepository.UpdateAsync(student);
+
+        return updatedStudent.Adapt<StudentDTO>();
+    }
+
+    public async ValueTask<StudentDTO> RemoveStudentAsync(Guid studentId)
+    {
+        var student = await _studentRepository.SelectByIdAsync(studentId);
+        //validate
+        var deletedStudent = await _studentRepository.DeleteAsync(student);
+
+        return deletedStudent.Adapt<StudentDTO>();
+    }
+
+    public async ValueTask<StudentDTO> RetrieveStudentByIdAsync(Guid studentId)
+    {
+        var student = await _studentRepository.SelectByIdAsync(studentId);
+        //validate
+
+        return student.Adapt<StudentDTO>();
+    }
+
+    public IQueryable<StudentDTO> RetrieveStudents()
+    {
+        var students = _studentRepository.SelectAll();
+
+        return students.Select(student => students.Adapt<StudentDTO>());
+    }
+}
