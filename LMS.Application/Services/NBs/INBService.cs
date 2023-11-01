2a715792594c9cb944e2dce84c8a64a2c9bc77ca
Sardor <107931170+SardorSohinazarov@users.noreply.github.com>
2023-11-01 21:07:32 +0500

    NBService implemeted (#42)

---
+++ LMS.Application/Services/NBs/INBService.cs
@@ -0,0 +1,12 @@
+﻿using LMS.Application.DTOs;
+
+namespace LMS.Application.Services.NBs;
+
+public interface INBService
+{
+    ValueTask<NBDTO> CreateNBAsync(NBForCreation nBForCreation);
+    IQueryable<NBDTO> RetrieveNBs();
+    ValueTask<NBDTO> RetrieveNBByIdAsync(Guid nBId);
+    ValueTask<NBDTO> ModifyNBAsync(NBForModification nBForModification);
+    ValueTask<NBDTO> RemoveNBAsync(Guid nBId);
+}
