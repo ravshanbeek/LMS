ab918d885a26829d051a97e4a39dbd7b41cb9746
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-11-16 13:48:25 +0500

    SubjectDto validation implemented

---
+++ LMS.Application/Validators/Subject/SubjectForCreationValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using LMS.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Application.Validators.Subject
+{
+    public class SubjectForCreationValidator : AbstractValidator<SubjectForCreation>
+    {
+        public SubjectForCreationValidator()
+        {
+            RuleFor(x => x.name)
+                .MaximumLength(50)
+                .NotEmpty();
+        }
+    }
+}
